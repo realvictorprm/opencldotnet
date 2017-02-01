ce55851d13621941f64aed0f53b7637b55306892
PRM_Victor <bubu278@web.de>
2017-02-01 10:07:21 +0100

    Started developing

---
+++ opencldotnet/OpenCL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace OpenCL
+{
+
+	public struct PlatformID
+	{
+	}
+
+	public class CL
+	{
+		public CL()
+		{
+		}
+
+
+		[DllImport("OpenCL.dll")]
+		public static extern void clGetPlatformIDs(uint maxPlatforms, PlatformID[] platforms, IntPtr count);
+	}
+
+}
